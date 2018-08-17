e4576ddd4da51137122787dbf23942d297fc32fa
unknown <330576744@qq.com>
2018-08-17 16:10:41 +0800

    init

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorillasss!\n"))
+}
+
+func main() {
+
+	http.HandleFunc("/", IndexHandler)
+	http.ListenAndServe("127.0.0.1:8080", nil)
+
+}
